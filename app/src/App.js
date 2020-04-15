00ad512bbb677861d16eeffc33117fcb9a0d9631
Isabella Chen <miss.izzyc@gmail.com>
2020-04-15 21:14:33 +0200

    react app with server

---
+++ app/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [greeting, setGreeting] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const greeting = await fetch(`/api/greeting?name=isabella`).then((res) =>
+        res.json()
+      );
+      setGreeting(greeting);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{greeting && greeting.greeting}</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
