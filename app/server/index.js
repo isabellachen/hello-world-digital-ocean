fd70f5329d011bc3141c83160cd6de926f6f3188
Isabella Chen <miss.izzyc@gmail.com>
2020-04-16 16:08:20 +0200

    enable cors

--- app/server/index.js
+++ app/server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
